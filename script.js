7230983fb66b4a988a84c513d89c1411dfc68468
musuu111 <mustafatariq5670@gmail.com>
2026-06-26 06:19:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function checkPassword() {
 
+    let pass = document.getElementById("password").value;
+
+    if(pass === "2512"){
+
+        window.location.href = "home.html";
+
+    }else{
+
+        document.getElementById("message").innerHTML = "❌ Wrong Password";
+
+    }
+
+}
